0550d3ceb091622a880e29f73c752f81605cb3be
Hernan Camilo Gomez Ruiz <gome109070@siigo.com>
2020-06-14 17:13:33 -0500

    Update Repository and Clean IoC

--- ClenaArch/CleanArch.Infra.IoC/DependencyContainer.cs
+++ ClenaArch/CleanArch.Infra.IoC/DependencyContainer.cs
@@ -1,7 +1,13 @@
 ﻿using CleanArch.Application.Interfaces;
 using CleanArch.Application.Services;
+using CleanArch.Domain.Core.Bus;
 using CleanArch.Infa.Data.Repositories;
+using CleanArch.Infra.Bus;
+using CleanArch.Infra.Data.Context;
+using CleanArh.Domain.CommandHandlers;
+using CleanArh.Domain.Commands;
 using CleanArh.Domain.Interfaces;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -13,11 +19,19 @@ namespace CleanArch.Infra.IoC
     {
         public static void RegisterServices(IServiceCollection services)
         {
+            //Domian InMemoryBus MediatR
+            services.AddScoped<IMediatorHandler, InMemoryBus>();
+
+            //Domain Handlers
+            services.AddScoped<IRequestHandler<CreateCourseCommand, bool>, CourseCommandHandler>();
+
             //Application Layer
             services.AddScoped<ICourseService, CourseService>();
 
             //Infra.Data Layer
             services.AddScoped<ICourseRepository, CourseRepository>();
+
+            services.AddScoped<UniversityDBContext>();
         }
     }
 }
